f1bb2a81c2b0bf22e70d837a15b3ab5e5e786156
Shishin Mo <maoson0307@gmail.com>
2019-03-28 17:44:47 +0900

    Added mpms curve display.

--- mpms/urls.py
+++ mpms/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 
 from rest_framework import routers
-from .views import DataFileViewSet, CurveViewSet
+from .views import DataFileViewSet, CurveViewSet, IndexView
 
 router = routers.DefaultRouter()
 router.register(r'curve', CurveViewSet)
 router.register(r'data', DataFileViewSet)
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+]
