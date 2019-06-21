55951cdb8f838db961dd14ab7959ccc38f63b122
Shishin Mo <maoson0307@gmail.com>
2019-06-21 17:22:45 +0900

    [WIP]Added SampleTable Component.

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -90,6 +90,92 @@ SampleTable.propTypes = {
     samples: PropTypes.array.isRequired
 };
 
+export class SampleTable_ extends React.Component {
+    static propTypes = {
+        samples: PropTypes.array
+    };
+
+    state = {
+        columns: [
+            {name: "Date", data: "fab_date"},
+            {name: "PLD", data: "pld"},
+            {name: "Target", data: "targets_string"},
+            {name: "Substrate", data: "substrate_abbreviation"},
+            {name: "Size", data: "sub_size"},
+            {name: "Mask?", data: "is_masked"},
+            {name: "Atmosphere", data: "atmosphere_gas"},
+            {name: "P<sub>Atmosphere</sub> (Torr)", data: "atmosphere_pressure"},
+            {name: "P<sub>Background</sub> (Torr)", data: "background_pressure"},
+            {name: "E<sub>Laser</sub> (mJ)", data: "laser_energy"},
+            {name: "Comment", data: "comment"},
+        ],
+        unused_columns: []
+    };
+
+    handleDeleteColumn = e => {
+        this.setState({
+            unused_columns: this.state.unused_columns.concat(
+                this.state.columns.filter(c => c.date === e.target.value)
+            ),
+            columns: update(this.state.columns.filter(c => c.data !== e.target.value))
+        })
+    };
+
+    handleAddColumn = e => {
+        this.setState({
+            columns: this.state.columns.concat(
+                this.state.unused_columns.filter(c => c.date === e.target.value)
+            ),
+            unused_columns: update(this.state.unused_columns.filter(c => c.data !== e.target.value))
+        })
+    };
+
+    render() {
+        const samples = this.props.samples;
+        const { columns, unused_columns } = this.state;
+        return (
+            !samples.length ? (
+                <p>Nothing to show</p>
+            ) : (
+                <div id="sample_list" className="columns">
+                    <div className="buttons are-small" id="unused_columns">
+                        {unused_columns.map(c => (
+                            <a className="button" onClick={this.handleAddColumn}>{c.name}</a>
+                        ))}
+                    </div>
+                    <div className="column">
+                        <h2 className="subtitle">
+                            Showing <strong>{samples.length} items</strong>
+                        </h2>
+                        <table className="table is-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Batch #</th>
+                                    {columns.map(c => (
+                                        <th>{c.name}</th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {samples.map(sample => (
+                                <tr key={sample.id}>
+                                    <td><a href={"/sample/" + sample.id}>{sample.id}</a></td>
+                                    <td><a href={"/api/batch/" + sample.batch_id}>{sample.pld_batch_id}</a></td>
+                                    {columns.map(c => (
+                                        <td>{sample[c.data]}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            )
+        )
+    }
+}
+
 export const TargetTable = ({ targets }) =>
     !targets.length ? (
         <p>Nothing to show</p>
